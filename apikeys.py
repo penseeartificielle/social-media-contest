cf95c8c558727c557568218722a6d92a599489b5
lrosique <lambertrosique@hotmail.com>
2018-03-20 00:44:28 +0100

    init

---
+++ apikeys.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# FACEBOOK
+TOKEN_USER_FB_SECRET = "Your user token"
+FB_USERNAME = "Your username from FB"
+FB_PASSWORD = "Your password from FB"
+# TWITTER
+TW_CONSUMER_KEY = "Twitter consumer key"
+TW_CONSUMER_SECRET = "Twitter consumer secret"
+TW_ACCESS_KEY = "Twitter access key"
+TW_ACCESS_SECRET = "Twitter access secret"
+TW_USERNAME = "Your username from Twitter"
+TW_PASSWORD = "Your password from Twitter"
